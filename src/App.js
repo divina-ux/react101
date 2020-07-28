d19574b97eab8d6c487249731c8ddb39f68cb600
divina <jacob.andrew.lepper@gmail.com>
2020-07-28 16:12:23 +1200

    Add and Delete notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      notes: [
+        {
+          id: 1,
+          text: 'Paint an illustration.'
+        },
+        {
+          id: 2,
+          text: 'Draw a user interface.'
+        },
+        {
+          id: 3,
+          text: 'App maker festival.'
+        },
+        {
+          id: 4,
+          text: 'Dua Lipa Auckland concert.'
+        },
+        {
+          id: 5,
+          text: 'Instrument recording, mixing.'
+        }
+      ],
+      noteInputValue : ''
+    }
+  
+  }
+
+  handleNoteInputChange = (e)=>{
+    this.setState({noteInputValue:e.target.value})
+    
+  }
+
+  handleAddButtonClick = (e)=>{
+    e.preventDefault()
+    var note = {
+      id:Date.now(),
+      text:this.state.noteInputValue
+    }
+
+    var newNotes = [note, ...this.state.notes]
+
+    this.setState({
+      notes:newNotes,
+      noteInputValue : ''
+    })
+  }
+
+  handleNoteDelete = (e)=>{
+    var noteIdToDelete = parseInt(e.target.id)
+    var notes = this.state.notes
+
+
+    var filteredNotes = notes.filter((item)=>{
+      return item.id != noteIdToDelete
+    })
+
+    this.setState({notes:filteredNotes})
+  }
+
+
+  render() {
+    return (
+      <div className="wrap">
+        <div className="container">
+          <h1>Hello<br />This is your to-do list.</h1>
+          <div className="notes">
+  
+            {
+              this.state.notes.map(note=>{
+                return (
+                  <div className="note" key={note.id}>
+                    <div className="note-body">
+                      <i id={note.id} className="far fa-times-circle note-remove" onClick={this.handleNoteDelete} ></i>
+                      <div className="note-text">
+                      {note.text}
+                      </div>
+                    </div>
+                  </div>
+              )
+            })
+          }
+  
+              <div className="note new-note">
+                  <div className="form-group">
+                      <label htmlFor="note-input">
+                          Add a new note
+                      </label>
+                      <input type="text" className="form-control" id="note-input" value={this.state.noteInputValue} onChange={this.handleNoteInputChange} />
+                  </div>
+  
+                  <button type="submit" className="btn btn-primary" onClick={this.handleAddButtonClick}> Add </button>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
